df9023b72586d398bd71e8a8bcaa91c92676f1d0
Maria <lrabin@gmail.com>
2019-07-05 17:10:31 -0400

    Initial files added

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
 var express = require("express");
 
-var PORT = process.env.PORT || 8081;
-
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = process.env.PORT || 7001;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse application body as JSON
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Static directory
+app.use(express.static("app/public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
-
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function () {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("App listening on PORT " + PORT);
 });
